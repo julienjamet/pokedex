212a36a61c767ddc64e22bf455b931284448d0a8
julienjamet <j.jamet@alphalink.fr>
2023-08-03 13:14:20 +0200

    correct evolve requirement

--- dist/models/pokemon.js
+++ dist/models/pokemon.js
@@ -9,7 +9,7 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const pokemon = new mongoose_1.default.Schema({
     number: { type: Number, unique: true, required: true },
     name: { type: String, unique: true, required: true },
-    evolve: { type: String, required: true },
+    evolve: { type: String },
     description: { type: String, unique: true, required: true },
     picture: { type: String, unique: true, required: true },
     type: { type: [String], required: true }
--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -6,7 +6,7 @@ const pokemon: Schema<IPokemon> = new mongoose.Schema(
     {
         number: { type: Number, unique: true, required: true },
         name: { type: String, unique: true, required: true },
-        evolve: { type: String, required: true },
+        evolve: { type: String },
         description: { type: String, unique: true, required: true },
         picture: { type: String, unique: true, required: true },
         type: { type: [String], required: true }
