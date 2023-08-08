3873b3e7e12369c95ce1a63d12eb60eb439d66fc
julienjamet <j.jamet@alphalink.fr>
2023-08-08 13:20:15 +0200

    pokedex design main page

--- frontend/src/pages/Pokedex.tsx
+++ frontend/src/pages/Pokedex.tsx
@@ -14,6 +14,59 @@ export const Pokedex: FC = () => {
 
             .catch((error: AxiosError): void => { console.log(error) })
     }, [pokedex])
+    /********************************************************Type design*/
+    const getTypeDesign = (type: string): string => {
+        switch (type) {
+            case "PLANTE":
+                return "plante"
+
+            case "FEU":
+                return "feu"
+
+            case "EAU":
+                return "eau"
+
+            case "ELECTRIK":
+                return "electrik"
+
+            case "PSY":
+                return "psy"
+
+            case "COMBAT":
+                return "combat"
+
+            case "VOL":
+                return "vol"
+
+            case "SOL":
+                return "sol"
+
+            case "ROCHE":
+                return "roche"
+
+            case "GLACE":
+                return "glace"
+
+            case "NORMAL":
+                return "normal"
+
+            case "POISON":
+                return "poison"
+
+            case "DRAGON":
+                return "dragon"
+
+            case "INSECTE":
+                return "insecte"
+
+            case "FÉE":
+                return "fée"
+
+            default:
+                return ""
+        }
+    }
+
     /*********************************************************Return TSX*/
     return (
         <>
@@ -21,20 +74,27 @@ export const Pokedex: FC = () => {
                 <div className="pokedex">
                     <h1>{pokedex.message}</h1>
 
+                    <div className="rank">
+                        {pokedex.rank && <h2>RANG : {pokedex.rank}</h2>}
+                        {pokedex.forNextRank && <h2>{pokedex.forNextRank}</h2>}
+                    </div>
+
                     <ul>
-                        {pokedex.pokedex.map((pokemon: IPokemon) => {
-                            return (
-                                <li key={pokemon._id} className="pokemon">
-                                    <span className="pokemonNumber">{pokemon.number}</span>
-                                    <span className="pokemonName">{pokemon.name}</span>
-                                    <img className="pokemonImage" src={pokemon.picture} alt={`${pokemon.name} cover`} />
-                                    <span className="pokemonDescription">{pokemon.description}</span>
-                                    <span className="pokemonType">{pokemon.type}</span>
-                                </li>
-                            )
-                        })}
+                        {pokedex.pokedex.map((pokemon: IPokemon): JSX.Element => (
+                            <li key={pokemon._id} className="pokemon">
+                                <span className="number" id={getTypeDesign(pokemon.type[0])}>{pokemon.number}</span>
+                                <span className="name">{pokemon.name}</span>
+                                <img className="image" src={pokemon.picture} alt={`${pokemon.name} cover`} />
+                                <span className="description">{pokemon.description}</span>
+                                <div className="types">
+                                    {pokemon.type.map((type: string, index: number): JSX.Element => (
+                                        <span key={index} id={getTypeDesign(type)}>{type}</span>
+                                    ))}
+                                </div>
+                            </li>
+                        ))}
                     </ul>
-                </div>
+                </div >
             ) : (
                 <div></div>
             )}
